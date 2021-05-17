2655a3908c17309a146c759c129d1bae1263eb30
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2021-05-16 23:09:56 -0300

    Roteamento para tela de login
    Agora é possível chamar a tela de login na rota

--- edutech/src/router/index.js
+++ edutech/src/router/index.js
@@ -1,12 +1,18 @@
 import VueRouter from 'vue-router'
 
 const Dashboard = () => import('../views/dashboard.vue')
+const Login = () => import('../views/login.vue')
 
 const routes = [
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/:pathMatch(.*)*',
--- edutech/src/views/login.vue
+++ edutech/src/views/login.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-      
+    <h1>Hello world of Login</h1>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: () => {
+    return {
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
