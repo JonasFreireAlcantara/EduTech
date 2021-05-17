82273c2bed0f6325bb0a19236ec81c581ded0f37
Wilder Santos Carvalho <wcarvalho98@gmail.com>
2021-05-16 23:25:48 -0300

    Roteando entre login e dashboard
    Roteando entre a tela de login e a tela de dashboard (apenas teste)

--- edutech/src/views/login.vue
+++ edutech/src/views/login.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    <h1>Hello world of Login</h1>
+    <h1 @click="goToHome">Edutech</h1>
   </div>
 </template>
 
 <script>
+import router from '../router'
+
 export default {
   data: () => {
     return {
     }
+  },
+  methods: {
+    goToHome: function () {
+      router.push({ name: 'Dashboard' })
+    }
   }
 }
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  padding: 16px 0px 0px;
+  cursor: pointer;
+}
+</style>
