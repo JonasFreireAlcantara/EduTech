aec32dc1ca4fbc7919e38f082207593168826f36
Jonas Freire <jonasfreireperson@gmail.com>
2021-05-21 00:21:11 -0300

    Addition of pomodoro modal
    Using VueModoro library the pomodoro was added.

---
+++ edutech/src/views/pomodoroModal.vue
@@ -0,0 +1,83 @@
+<template>
+  <b-modal id="pomodoroModal" title="Pomodoro" size="xl" hide-footer header-class="text-dark" header-close-content="<h2>&times;</h2>" scrollable>
+
+    <b-tabs content-class="mt-2" nav-class="text-dark" pills fill>
+      <b-tab title="Pomodoro" title-link-class="tab" active>
+        <div class="pomodoro-wrapper">
+          <VueModoro theme="light"
+            pomodoro-label="Pomodoro"
+            start-label="Iniciar"
+            pause-label="Parar"
+            reset-label="Reiniciar"/>
+        </div>
+      </b-tab>
+
+      <b-tab title="Pausa Curta" title-link-class="tab">
+        <div class="pomodoro-wrapper">
+          <VueModoro theme="light"
+            pomodoro-label="Pausa curta"
+            start-label="Iniciar"
+            pause-label="Parar"
+            reset-label="Reiniciar"
+            :minutes="10" />
+        </div>
+      </b-tab>
+
+      <b-tab title="Pausa Longa" title-link-class="tab">
+        <div class="pomodoro-wrapper">
+          <VueModoro theme="light"
+            pomodoro-label="Pausa longa"
+            start-label="Iniciar"
+            pause-label="Parar"
+            reset-label="Reiniciar"
+            :minutes="25" />
+        </div>
+      </b-tab>
+    </b-tabs>
+  </b-modal>
+</template>
+
+<script>
+import VueModoro from 'vuemodoro'
+
+export default ({
+  components: {
+    VueModoro
+  }
+})
+</script>
+
+<style>
+  @media (min-width: 1300px) {
+    .modal-xl {
+      max-width: 1270px;
+    }
+  }
+  .tab {
+    color: gray;
+    background-color: white;
+    font-size: larger;
+    max-width: 200px;
+    margin: 0 auto;
+  }
+  .nav-pills .nav-link.active.tab {
+    color: #2E294E;
+    background-color: white;
+    font-weight: 600;
+  }
+  .nav-link:hover {
+    color: #2E294E;
+  }
+  .nav-link.active::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 10px;
+    border-bottom: 3px solid;
+    margin: 0 auto;
+    color: #2E294E;
+  }
+  .pomodoro-wrapper {
+    padding: 50px;
+  }
+</style>
--- edutech/src/views/workspace.vue
+++ edutech/src/views/workspace.vue
@@ -3,6 +3,7 @@
     <TopComponent :isLogged="true"/>
     <b-container fluid class="margin-top">
       <NextEvents/>
+      <PomodoroModal/>
       <b-row>
         <b-col md="5" sm="12" class="mt-5 component-bg mx-auto p-3 shadow rounded">
           <b-row>
@@ -42,16 +43,21 @@
             </b-col>
           </b-row>
         </b-col>
+
         <b-col md="5" sm="12" class="mt-5 component-bg mx-auto p-3 shadow rounded">
           <b-row>
             <b-col cols="10">
               <h2>Tempo</h2>
             </b-col>
             <b-col cols="2" class="text-center">
-              <a><h2><b-icon icon="arrows-angle-expand"></b-icon></h2></a>
+              <a v-b-modal.pomodoroModal><h2><b-icon icon="arrows-angle-expand"></b-icon></h2></a>
             </b-col>
           </b-row>
+          <b-row class="mt-4">
+            <h4>Implemented modal</h4>
+          </b-row>
         </b-col>
+
       </b-row>
     </b-container>
   </div>
@@ -60,6 +66,7 @@
 <script>
 import NextEvents from './nextEvents.vue'
 import Event from './event.vue'
+import PomodoroModal from './pomodoroModal'
 import TopComponent from '../components/top-component.vue'
 
 export default ({
@@ -95,6 +102,7 @@ export default ({
   components: {
     NextEvents,
     Event,
+    PomodoroModal,
     TopComponent
   }
 })
